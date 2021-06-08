8bf6ab3f6b6b90412fae83530d63aa76e9461294
Michael Lustenberger <mic@inofix.ch>
2021-06-08 02:30:41 +0200

    Cobra / Viper
    As we do plan to use config files and a proper cli.

---
+++ cmd/root.go
@@ -0,0 +1,81 @@
+/*
+This is Free Software; feel free to redistribute and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+Copyright © 2021 Michael Lustenberger <mic@inofix.ch>
+*/
+
+package cmd
+
+import (
+    "fmt"
+    "os"
+    "github.com/spf13/cobra"
+    homedir "github.com/mitchellh/go-homedir"
+    "github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+    Use:   "natem",
+    Short: "Natem helps organizing information in a knowledge base system",
+    Long: `Natem allows you to access and edit your knowledge, stored in
+(e.g. different, distributed) repositories by combining that content based
+on it's config file into a mighty source of information and presenting it
+using specific views, depending on your current focus.`,
+
+    // Uncomment the following line if your bare application
+    // has an action associated with it:
+//    Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+    cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+    cobra.OnInitialize(initConfig)
+
+    // Here you will define your flags and configuration settings.
+    // Cobra supports persistent flags, which, if defined here,
+    // will be global for your application.
+
+    rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.natem.yaml)")
+
+    // Cobra also supports local flags, which will only run
+    // when this action is called directly.
+//    rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+    if cfgFile != "" {
+        // Use config file from the flag.
+        viper.SetConfigFile(cfgFile)
+    } else {
+        // Find home directory.
+        home, err := homedir.Dir()
+        cobra.CheckErr(err)
+
+        // Search config in home directory with name ".natem" (without extension).
+        viper.AddConfigPath(home)
+        viper.SetConfigName(".natem")
+    }
+
+    viper.AutomaticEnv() // read in environment variables that match
+
+    // If a config file is found, read it in.
+    if err := viper.ReadInConfig(); err == nil {
+        fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+    }
+}
