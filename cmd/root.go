c3457091664978f4262fefbad2de3b3bffac5c33
Michael Lustenberger <mic@inofix.ch>
2021-06-27 03:03:31 +0200

    Clean Up
    More clean up (by vim-go).

--- cmd/root.go
+++ cmd/root.go
@@ -14,68 +14,69 @@ Copyright © 2021 Michael Lustenberger <mic@inofix.ch>
 package cmd
 
 import (
-    "fmt"
-    "os"
-    "github.com/spf13/cobra"
-    homedir "github.com/mitchellh/go-homedir"
-    "github.com/spf13/viper"
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-    Use:   "natem",
-    Short: "Natem helps organizing information in a knowledge base system",
-    Long: `Natem allows you to access and edit your knowledge, stored in
+	Use:   "natem",
+	Short: "Natem helps organizing information in a knowledge base system",
+	Long: `Natem allows you to access and edit your knowledge, stored in
 (e.g. different, distributed) repositories by combining that content based
 on it's config file into a mighty source of information and presenting it
 using specific views, depending on your current focus.`,
 
-    // Uncomment the following line if your bare application
-    // has an action associated with it:
-//    Run: func(cmd *cobra.Command, args []string) { },
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//    Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-    cobra.CheckErr(rootCmd.Execute())
+	cobra.CheckErr(rootCmd.Execute())
 }
 
 func init() {
-    cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(initConfig)
 
-    // Here you will define your flags and configuration settings.
-    // Cobra supports persistent flags, which, if defined here,
-    // will be global for your application.
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
 
-    rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.natem.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.natem.yaml)")
 
-    // Cobra also supports local flags, which will only run
-    // when this action is called directly.
-//    rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	//    rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-    if cfgFile != "" {
-        // Use config file from the flag.
-        viper.SetConfigFile(cfgFile)
-    } else {
-        // Find home directory.
-        home, err := homedir.Dir()
-        cobra.CheckErr(err)
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		cobra.CheckErr(err)
 
-        // Search config in home directory with name ".natem" (without extension).
-        viper.AddConfigPath(home)
-        viper.SetConfigName(".natem")
-    }
+		// Search config in home directory with name ".natem" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".natem")
+	}
 
-    viper.AutomaticEnv() // read in environment variables that match
+	viper.AutomaticEnv() // read in environment variables that match
 
-    // If a config file is found, read it in.
-    if err := viper.ReadInConfig(); err == nil {
-        fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-    }
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
 }
