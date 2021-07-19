e24f0d8c561057ae1dbbb3b900d256bdf3de5420
Michael Lustenberger <mic@inofix.ch>
2021-07-19 13:05:55 +0200

    Testing
    Test the plain help flags.

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -14,10 +14,31 @@ Copyright © 2021 Michael Lustenberger <mic@inofix.ch>
 package cmd
 
 import (
+	"bytes"
+	"io/ioutil"
 	"testing"
 )
 
-func Test_ExecuteCommand(t *testing.T) {
+// Test the basics...
+func TestExecuteHelp(t *testing.T) {
 	cmd := NewRootCmd()
+	a := bytes.NewBufferString("")
+	b := bytes.NewBufferString("")
+	cmd.SetOut(a)
+	cmd.SetArgs([]string{"help"})
 	cmd.Execute()
+	aOut, err := ioutil.ReadAll(a)
+	if err != nil {
+		t.Fatal(err)
+	}
+	cmd.SetOut(b)
+	cmd.SetArgs([]string{"--help"})
+	cmd.Execute()
+	bOut, err := ioutil.ReadAll(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(aOut) != string(bOut) {
+		t.Fatalf("expected the same output for `help` and `--help`, but got ...\n\"%s\"\n ... and ... \n\"%s\"", string(aOut), string(bOut))
+	}
 }
