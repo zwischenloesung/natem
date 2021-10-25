ac1e449a44dda5cc11ffe6ce4fcb28512172d95a
Michael Lustenberger <mic@inofix.ch>
2021-10-25 22:23:19 +0200

    Simplify
    Throw away unnecessary part.

--- util/url.go
+++ util/url.go
@@ -13,31 +13,23 @@ Copyright © 2021 Michael Lustenberger <mic@inofix.ch>
 package util
 
 import (
-	"log"
+	"errors"
 	"net/url"
 )
 
-type URI struct {
-	*url.URL
-}
-
 type ThingURL struct {
-	URI
+	*url.URL
 	Context string
 }
 
 var SupportedThingURLSchemes = []string{"file", "http", "https"}
 
-func ParseURI(u string) (*URI, error) {
+func ParseThingURL(u string, context string) (*ThingURL, error) {
 	uri, err := url.Parse(u)
 	if err != nil {
-		log.Fatal("An error occured while parsing the URI.\n", err)
+		return nil, err
 	}
-	return &URI{uri}, err
-}
 
-func ParseThingURL(u string, context string) (*ThingURL, error) {
-	uri, err := ParseURI(u)
 	// Only consider URLs pointing to the local file system, allow for
 	// absolute or relative paths too.
 	if uri.Scheme == "file" || uri.Scheme == "" {
@@ -45,10 +37,9 @@ func ParseThingURL(u string, context string) (*ThingURL, error) {
 		if uri.Path[0] != byte('/') {
 			uri.Path = context + "/" + uri.Path
 		}
-		return &ThingURL{*uri, context}, nil
+		return &ThingURL{uri, context}, nil
 	} else {
-		log.Fatal("This URL was not of scheme 'file:///' as expected.\n", err)
-		return &ThingURL{*uri, context}, err
+		return &ThingURL{uri, context}, errors.New("This URL was not of scheme 'file:///' as expected.\n")
 	}
 }
 
--- util/url_test.go
+++ util/url_test.go
@@ -17,46 +17,6 @@ import (
 	"testing"
 )
 
-// TODO: ATM some of the tests here are almost like 'testing upstream'
-// (which we will have to clean up later), the idea is to test the basics
-// here first to make sure the later tests succeed/fail for the right reasons.
-func TestParseURI(t *testing.T) {
-	a := "urn:uuid:00000000-0000-0000-0000-000000000000"
-	b, e := ParseURI(a)
-	if e != nil {
-		t.Fatal("parsing the URI (urn) did not work as expected")
-	}
-	if b.Scheme != "urn" {
-		t.Fatal("setting the 'Scheme' value did not work as expected while parsing the URI (urn)")
-	}
-	a = "https://example.org"
-	b, e = ParseURI(a)
-	if e != nil {
-		t.Fatal("parsing the URI (https) did not work as expected")
-	}
-	if b.Host != "example.org" {
-		t.Fatal("setting the 'Host' value did not work as expected while parsing the URI (https)")
-	}
-	if e != nil {
-		t.Fatal("parsing the URI (https) did not work as expected")
-	}
-	if b.Path != "" {
-		t.Fatal("'Path' value is expected to be empty while parsing this URI (https)")
-	}
-	b.Path = "foo/bar.html"
-	if b.String() != "https://example.org/foo/bar.html" {
-		t.Fatal("manually setting the 'Path' value did not work as expected for URI (https)")
-	}
-	a = "tag:test@example.org,1970:foobar"
-	b, e = ParseURI(a)
-	if e != nil {
-		t.Fatal("parsing the URI (tag) did not work as expected")
-	}
-	if b.Opaque != "test@example.org,1970:foobar" {
-		t.Fatalf("The value of `Opaque` is not as expected: %s.\n", b.Opaque)
-	}
-}
-
 func TestParseThingURL(t *testing.T) {
 	a := "testing/example.yml"
 	p := "/home/foo"
